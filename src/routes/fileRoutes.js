351fa59ea8614158d66e667ff18fd1a85c1a8e4e
Srujana-Adapa <adapa.s@northeastern.edu>
2025-03-19 17:49:04 -0400

    fixed workflow issues

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
-const multerS3 = require('multer-s3');
-const { fromIni } = require('@aws-sdk/credential-provider-ini');
+// const multerS3 = require('multer-s3');
+// const { fromIni } = require('@aws-sdk/credential-provider-ini');
 const { File } = require('../models/file'); 
 const { v4: uuidv4 } = require('uuid');
 const { S3Client, PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
