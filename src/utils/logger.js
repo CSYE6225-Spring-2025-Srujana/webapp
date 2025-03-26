40eff421a2771c6ab129b9dac985e1b850713672
Srujana-Adapa <adapa.s@northeastern.edu>
2025-03-25 23:11:24 -0400

    added cloud watch configurations

---
+++ src/utils/logger.js
@@ -0,0 +1,44 @@
+const winston = require('winston');
+const { combine, timestamp, json } = winston.format;
+ const StatsD = require('hot-shots');
+
+const statsd = new StatsD({
+    host: '127.0.0.1',
+    port: 8125,
+    prefix: 'webapp.',
+  });
+
+
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console(),  // Logs to the console
+        // new winston.transports.File({ filename: '/home/ubuntu/webapp/logs/webapp.log' }) // Logs to a file
+        new winston.transports.File({ filename: '/opt/csye6225/logs/webapp.log' }) 
+    ],
+});
+
+// Log API call metrics
+function logApiCall(apiName, duration) {
+    statsd.increment(`api.calls.${apiName}`); 
+    statsd.timing(`api.timing.${apiName}`, duration);
+}
+
+// Log database query metrics
+function logDbQuery(duration) {
+    statsd.timing('db.query.timing', duration);
+}
+
+// Log S3 service call metrics
+function logS3Call(duration) {
+    statsd.timing('s3.call.timing', duration);
+}
+
+module.exports = { logger, logApiCall, logDbQuery, logS3Call };
+
